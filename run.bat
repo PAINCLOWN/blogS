c5d6596f6b998b4386c0c94929d0742f1bb8cf4a
PAINCLOWN <pozz@vip.qq.com>
2018-12-14 18:40:05 +0800

    2018年12月14日

--- run.bat
+++ run.bat
@@ -9,7 +9,7 @@ call  hexo_clean.bat
 echo;
 echo 清空完成
 echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-pause
+
 
 echo;
 echo 生成静态页面
@@ -19,7 +19,7 @@ call hexo_g.bat
 echo;
 echo 生成完成
 echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-pause
+
 
 echo;
 echo 同步到远程blog仓库
@@ -29,7 +29,7 @@ call hexo_d.bat
 echo;
 echo 同步完成
 echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-pause
+
 
 echo;
 echo 同步hexo到blogS仓库
